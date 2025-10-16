a5af7e2161311d4a8e600ccdfb9b3f11dda3f058
Miaoshang <youcai233@gmail.com>
2025-10-16 10:29:13 +0800

    rm unused file

--- nextjs/next-intl.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-intl').NextIntlConfig} */
-module.exports = {
-  locales: ['ja', 'en'],     // add 'zh' etc. when ready
-  defaultLocale: 'ja',
-  messages: {
-    ja: () => import('./messages/ja.json'),
-    en: () => import('./messages/en.json')
-  }
-};
